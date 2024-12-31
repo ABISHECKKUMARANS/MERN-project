8f83c76e1e84a9f7ba57f76a8d9e592aebe89c5a
Abisheckkumaran S <146187010+ABISHECKKUMARANS@users.noreply.github.com>
2024-12-31 06:14:18 +0530

    DAY4-2

--- abifirst/src/main.jsx
+++ abifirst/src/main.jsx
@@ -1,9 +1,10 @@
-  import { StrictMode } from 'react'
-  import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import Abi from './Abi.jsx'
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <Abi />
-    </StrictMode>
-  )
-
+import List from './List.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Abi />
+    {/* <List/> */}
+  </StrictMode>
+)
